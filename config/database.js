1bc9cfecb49416c84447ab67c3854a34aa74412a
Brenno Henrique <brennovicentini@DESKTOP-N8G6UTQ.localdomain>
2021-08-26 15:25:21 +0100

    update ssl and option in database.js

--- config/database.js
+++ config/database.js
@@ -10,9 +10,13 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'brenin22'),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: {
+          rejectUnauthorized: false
+        }
       },
-      options: {}
+      options: {
+        ssl: true,
+      }
     },
   },
 });
